d631e297f9f9e321fb5c8e0843fff65d13e056f4
TesnSai <shivamchaudhary8476@gmail.com>
2021-08-03 12:59:00 +0530

    login activity added

---
+++ app/src/main/java/com/example/materialapp1/LoginActivity.java
@@ -0,0 +1,18 @@
+package com.example.materialapp1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Build;
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            getWindow().setNavigationBarColor(getResources().getColor(R.color.appprimary));
+        }
+    }
+}
