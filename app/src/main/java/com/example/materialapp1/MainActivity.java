c645b48a4464abc40b40860d8489d8892a16a348
TesnSai <shivamchaudhary8476@gmail.com>
2021-08-05 22:46:10 +0530

    Navigation added to Activities

--- app/src/main/java/com/example/materialapp1/MainActivity.java
+++ app/src/main/java/com/example/materialapp1/MainActivity.java
@@ -2,23 +2,57 @@ package com.example.materialapp1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button switchToSecondActivity;
+    Button switchToThirdActivity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         View decorView = getWindow().getDecorView();
 // Hide both the navigation bar and the status bar.
 // SYSTEM_UI_FLAG_FULLSCREEN is only available on Android 4.1 and higher, but as
-// a general rule, you should design your app to hide the status bar whenever you
-// hide the navigation bar.
+
         int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
                 | View.SYSTEM_UI_FLAG_FULLSCREEN;
         decorView.setSystemUiVisibility(uiOptions);
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        switchToSecondActivity = findViewById(R.id.loginButton);
+        switchToThirdActivity = findViewById(R.id.signupButton);
+
+
+        switchToSecondActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                switchActivities();
+            }
+        });
+
+        switchToThirdActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                switchActivities2();
+            }
+        });
+
+    }
+
+    private void switchActivities(){
+        Intent switchActivityIntent = new Intent(this,LoginActivity.class);
+        startActivity(switchActivityIntent);
+    }
+
+    private void switchActivities2(){
+        Intent switchActivityIntent = new Intent(this,SignUpActivity.class);
+        startActivity(switchActivityIntent);
+
     }
 }
